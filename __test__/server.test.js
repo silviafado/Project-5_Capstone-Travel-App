e34721e72de5a2ed97e7f64b05ab99ec1674d390
silviafado <silviafado@gmail.com>
2021-01-07 10:43:56 +0100

    Separate server-app files

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,10 +1,31 @@
-
-const app = require("./../src/server/server"); // Link to your server file
-const supertest = require("supertest");
+// Require server file
+const app = require('./../src/server/server'); 
+const supertest = require('supertest');
 const request = supertest(app);
+const trip = {
+  formDestination: 'Barcelona',
+};
 
-describe("Test API endpoints", () => {
-    test("GET / -> Successfuly render index.html", async () => {
-      const response = await request.get("/").expect(200);
+describe('Test API routes', () => {
+    test('GET Successfuly render the homepage', async () => {
+      const response = await request.get('/').expect(200);
     });
-  });
+    /*test('GET Successfuly get trips', async () => {
+      const response = await request.get('/data');
+      expect(response.statusCode).toBe(200);
+    });
+    test('POST Successfuly create a trip', async () => {
+      const response = await request
+        .post('/addEntry')
+        .send({ trip })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200); 
+      expect(response.body[0].city).toBe(trip.city);
+      expect(response.body[0].id).toBeTruthy();
+    });
+    test('GET Successfuly get trips', async () => {
+      const response = await request.get('/data').expect(200);
+      expect(response.body.length).toBe(1);
+    });*/
+});
--- src/server/app.js
+++ src/server/app.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
 // Define fetch in NodeJS
 const fetch = require('node-fetch');
