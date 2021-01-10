4f29073f8b392819be828a57e0dd53b6afd99f65
silviafado <silviafado@gmail.com>
2021-01-10 21:02:02 +0100

    test adjusted

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -10,22 +10,4 @@ describe('Test API routes', () => {
     test('GET Successfuly render the homepage', async () => {
       const response = await request.get('/').expect(200);
     });
-    /*test('GET Successfuly get trips', async () => {
-      const response = await request.get('/data');
-      expect(response.statusCode).toBe(200);
-    });
-    test('POST Successfuly create a trip', async () => {
-      const response = await request
-        .post('/addEntry')
-        .send({ trip })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200); 
-      expect(response.body[0].city).toBe(trip.city);
-      expect(response.body[0].id).toBeTruthy();
-    });
-    test('GET Successfuly get trips', async () => {
-      const response = await request.get('/data').expect(200);
-      expect(response.body.length).toBe(1);
-    });*/
 });
