92fdd3069289abf99658e0c580b40744684a4c7e
silviafado <silviafado@gmail.com>
2021-01-11 12:31:59 +0100

    Slide transition and UI layout

--- __test__/formHandler.test.js
+++ __test__/formHandler.test.js
@@ -1,16 +1,20 @@
 import { performAction, postGeo, postWeather, postPix } from '../src/client/js/formHandler';
 
 describe("Test that the functions are defined", () => {
-  test("performAction should be defined", async () => {
+  test("performAction should be defined", async (done) => {
     expect(performAction).toBeDefined();
+    done();
   });
-  test("postGeo should be defined", async () => {
+  test("postGeo should be defined", async (done) => {
     expect(postGeo).toBeDefined();
+    done();
   });
-  test("postWeather should be defined", async () => {
+  test("postWeather should be defined", async (done) => {
     expect(postWeather).toBeDefined();
+    done();
   });
-  test("postPix should be defined", async () => {
+  test("postPix should be defined", async (done) => {
     expect(postPix).toBeDefined();
+    done();
   });
 });
--- __test__/server.test.js
+++ __test__/server.test.js
@@ -2,12 +2,10 @@
 const app = require('./../src/server/server'); 
 const supertest = require('supertest');
 const request = supertest(app);
-const trip = {
-  formDestination: 'Barcelona',
-};
 
 describe('Test API routes', () => {
-    test('GET Successfuly render the homepage', async () => {
+    test('GET Successfuly render the homepage', async (done) => {
       const response = await request.get('/').expect(200);
+      done();
     });
 });
